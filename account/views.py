c483da6d2c95b7c40af464f27bb871220a9581c3
Ismail S. Dawjee <ismail.dawjee@gmail.com>
2025-08-29 02:11:37 +0200

    Create first views and templates for login a user in. Using django authentication framework, creates user_login function with URLs for logging a user in

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            username, password = cd["username"], cd["password"]
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated Successfully")
+                else:
+                    return HttpResponse("Disabled Account (Not active)")
+            else:
+                return HttpResponse("Invalid Login Details")
+    else:
+        form = LoginForm()
+
+    context = {"form": form}
+    return render(request, "account/login.html", context)
